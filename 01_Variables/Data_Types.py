dfd68489e8a533cc729451bd6cdc68c4bb0e1509
Vivek <vivrk2989@gmail.com>
2022-01-24 22:00:04 +0000

    Finished data types

---
+++ 01_Variables/Data_Types.py
@@ -0,0 +1,126 @@
+# Numeric!
+#
+# i = 375  #int
+# f = 3.75  #float
+# c = 3j + 2  #complex
+#
+# print(c, type(c))
+#
+# add = 3 + 5
+# subtract = 3 - 5
+# multiply = 3 * 5
+# divide = 3 / 5
+# power = 3 ** 5
+# modulo = 3 % 5
+#
+# print(13 % 3)  # 4* 3 = 12, 13 -12 = 1
+# print(12 % 3)
+#
+# print(math.sqrt(25))
+
+# Strings!
+
+# single = 'String in single quotes'
+# double = 'String in double quotes'
+#
+# single_in_double = "This is Vivek's string"
+# double_in_single = 'This is a "string"'
+# both = "This is Vivek's \"string\""
+# print(both)
+
+# # Indexing and slicing
+#
+# hi = "Hello World!"
+# print(hi[0])  # Python starts counting at Zero!
+# print(hi[6])
+# print(hi[-1])
+# print(hi[0:7])
+# print(hi[3:8])
+
+# Methods
+#
+# white_space = "    lots of white space    "
+# print(len(white_space))
+# print(white_space.strip())
+# print(white_space.upper())
+# print(white_space.strip().capitalize())
+# print(white_space.count(" "))
+# print(white_space.replace("o", "ooo").replace("i", "0000").replace("a", "00"))
+
+# # F-strings (formatting strings)
+#
+# pi = 3.14159265359
+# print(pi)
+# print(f"pi to 3dp: {pi:.3f}")
+# print(f"Pi to 5dp: {pi:.5f}")
+# print(f"Pi to 0dp: {pi:.0f}")
+
+# score = 16
+# max_score = 26
+#
+# print(f"You scored {score / max_score}")
+# print(f"You scored {score / max_score:.2f}")
+# print(f"You scored {score / max_score:%}")
+# print(f"You scored {score / max_score:.2%}")
+# print(f"You scored {score / max_score:.0%}")
+
+# Boolean
+
+# t = True
+# f = False
+#
+# print(t, type(t))
+#
+# print(3 + 2 == 5)
+# print(12 % 3 ==0)
+# print(5 != 5)
+# print(1 < 100)
+# print(100 < 1)
+# print(5 < 5)
+# print(5 <= 5)
+# print(5 >= 5)
+
+# age = 16
+# drink = 'alcohol'
+#
+# print(age > 18 and drink == 'alcohol')
+# print(age > 18 or drink == 'alcohol')
+#
+# hi = 'Hello World'
+# print(hi.isalpha())
+# print(hi.islower())
+# print(hi.isupper())
+# print(hi.endswith("rld"))
+# print(hi.startswith("Hell"))
+
+# print(bool(1))
+# print(bool(0))
+# print(bool(2342))
+# print(bool(-2342))
+# print(bool(1.342))
+
+# print(bool("Hello"))
+# empty = ""
+#
+# print(empty, type(empty), bool(empty))
+
+# None
+
+n = None
+
+print(n, type(n))
+print(bool(None))
+print(n is None)
+print(type(15) is int)
+
+
+
+
+
+
+
+
+
+
+
+
