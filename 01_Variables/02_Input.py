eed470d85ee9d77f30009c52e6f6c16fe292adab
Vivek <vivrk2989@gmail.com>
2022-01-24 21:09:44 +0000

    Added input session

---
+++ 01_Variables/02_Input.py
@@ -0,0 +1,8 @@
+name = input("what is your name?\n")
+
+print("Hello, " + name)
+
+age = int(input("How old are you?\n"))
+# print("You are"  + str(age) + "years old")
+print(f"You are {age} years old")  # formatted string
+print(f"Next year you will be {age + 1} years old")
