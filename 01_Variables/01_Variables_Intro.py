6262f156b8bd4c6bbbf7d351c4b4aa27251f062a
Vivek <vivrk2989@gmail.com>
2022-01-24 21:00:00 +0000

    Variables intro

---
+++ 01_Variables/01_Variables_Intro.py
@@ -0,0 +1,13 @@
+a = 1  # int (Integer)
+b = 2  # int
+c = 3.5  # float (floating-point number)
+d = "Hello"  # string
+
+result = str((a + c) * b)  # calling a function
+print(d)
+print()
+
+# Python is dynamically typed
+
+print(result, type(result))
+print(d, type(d))
