95aa1e76f7cda16a81aee18367bc26775307d0a5
Vivek <vivrk2989@gmail.com>
2022-01-26 03:06:59 +0000

    Added comments to FuzzBizz with functions

--- 03_Control_Flow/03_FizzBuzz.py
+++ 03_Control_Flow/03_FizzBuzz.py
@@ -4,20 +4,20 @@
 def FizzBuzz_game():
     Start_number = int(input('start value\n'))  # User input for start number
     End_number = int(input('End value\n'))  # User input for end number
-    div_by_3 = input('How would you want to describe the number if divisible by 3?\n')
-    div_by_5 = input('How would you want to describe the number if divisible by 5?\n')
-    div_by_3_and_5 = input('How would you want to describe the number if divisible by both 3 and 5?\n')
+    div_by_3 = input('How would you want to describe the number if divisible by 3?\n')  #User input for the word if number is divisible by 3
+    div_by_5 = input('How would you want to describe the number if divisible by 5?\n')  #User input for the word if number is divisible by 5
+    div_by_3_and_5 = input('How would you want to describe the number if divisible by both 3 and 5?\n')  #User input for the word if number is divisible by both 3 and 5
 
     for number in range(Start_number, End_number):
-        if number % 3 == 0 and number % 5 == 0:
+        if number % 3 == 0 and number % 5 == 0:  # If number is a multiple of both 3 and 5
             print(div_by_3_and_5)
-        elif number % 3 == 0:
+        elif number % 3 == 0:  # If the number is a multiple of 3
             print(div_by_3)
-        elif number % 5 == 0:
+        elif number % 5 == 0:  # If the number is a multiple of 5
             print(div_by_5)
-        else:
+        else:  # If it doesnt satisfy any of the above cases, we simply print this number
             print(number)
     return
 
 
-FizzBuzz_game()
+FizzBuzz_game()  #Calling functions
